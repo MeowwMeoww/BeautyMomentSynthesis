724a25cf58f79976c45ac6c32760ffb2dc05b54c
Hoang Phuc Nguyen <hoangphuc129203@gmail.com>
2022-05-03 18:42:58 +0700

    Update variables

--- SDD_FIQA.py
+++ SDD_FIQA.py
@@ -14,52 +14,51 @@ model_path = CFG_FIQA.MODEL_PATH
 
 
 def process_fiqa_image(img):
-
     data = torch.randn(1, 3, 112, 112)
-
     transform = T.Compose([
         T.Resize((112, 112)),
         T.ToTensor(),
         T.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
     ])
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    im_pil = Image.fromarray(img)
-    data[0, :, :, :] = transform(im_pil)
+    img_pil = Image.fromarray(img)
+    data[0, :, :, :] = transform(img_pil)
 
     return data
 
 
 def FIQA_network():
-    model_path  = CFG_FIQA.MODEL_PATH
+    model_path = CFG_FIQA.MODEL_PATH
     device = config.DEVICE
-    net = model.R50([112, 112], use_type="Qua").to(device)
-    net_dict = net.state_dict()
+    network = model.R50([112, 112], use_type="Qua").to(device)
+    net_dict = network.state_dict()
     data_dict = {
         key.replace('module.', ''): value for key, value in torch.load(model_path, map_location=device).items()}
     net_dict.update(data_dict)
-    net.load_state_dict(net_dict)
-    net.eval()
+    network.load_state_dict(net_dict)
+    network.eval()
 
-    return net
+    return network
 
 
-def FIQA(df, img_list, net):
+def FIQA(df, img_list, network):
     fiqa_scores = []
     keep_index = []
 
-    for i in range(len(df)):
-        input_data = get_target_bbox(img_list[i], df["bboxes"][i], p=CFG_FIQA.EXTEND_RATE)
+    for img_index in range(len(df)):
+        input_data = get_target_bbox(img_list[img_index], df["bboxes"][img_index], p=CFG_FIQA.EXTEND_RATE)
         scores = []
-        for j in input_data:
-            if j.shape[0] > 0 and j.shape[1] > 0:
-                img = process_fiqa_image(j).to(device)
-                pred_score = net(img).data.cpu().numpy().squeeze()
-                scores.append(pred_score)
+
+        for cropped_face in input_data:
+            img = process_fiqa_image(cropped_face).to(device)
+            pred_score = network(img).data.cpu().numpy().squeeze()
+            scores.append(pred_score)
+
         scores = [score.item() for score in scores]
         scores_avg = sum(scores)/len(scores)
-    #    if max(scores) > CFG_FIQA.THRESHOLD:
+
         if scores_avg >= CFG_FIQA.THRESHOLD:
-            keep_index.append(i)
+            keep_index.append(img_index)
             fiqa_scores.append([[score] for score in scores])
 
     new_df = df.iloc[keep_index]
